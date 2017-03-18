f6eba0b47ad42e8723d5f8dbb8ed32f3bf192c9c
fx <1091381572@qq.com>
2017-03-18 22:58:47 +0800

    首次提交

--- app/src/main/java/com/fx/soy/adapter/ItemAdapter.java
+++ app/src/main/java/com/fx/soy/adapter/ItemAdapter.java
@@ -1,9 +1,11 @@
 package com.fx.soy.adapter;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,45 +27,57 @@ public class ItemAdapter extends ArrayAdapter<Soy> {
 
     private int resourceId;
 
+    private Context mContext;
+
     List<Soy> mList;
 
     public ItemAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Soy> objects) {
         super(context, resource, objects);
-        resourceId=resource;
-        mList=objects;
+        mContext = context;
+        resourceId = resource;
+        mList = objects;
     }
 
     @NonNull
     @Override
     public View getView(final int position, @Nullable final View convertView, @NonNull final ViewGroup parent) {
-        final Soy soy =getItem(position);
+        final Soy soy = getItem(position);
         View view;
         ViewHolder viewHolder;
-        if (convertView==null){
-            view=LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
-            viewHolder=new ViewHolder();
-            viewHolder.date= (TextView) view.findViewById(R.id.show_date);
-            viewHolder.name= (TextView) view.findViewById(R.id.show_name);
-            viewHolder.delete= (Button) view.findViewById(R.id.bt_delete);
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.date = (TextView) view.findViewById(R.id.show_date);
+            viewHolder.name = (TextView) view.findViewById(R.id.show_name);
+            viewHolder.delete = (Button) view.findViewById(R.id.bt_delete);
             view.setTag(viewHolder);
-        }else {
-            view=convertView;
-            viewHolder= (ViewHolder) view.getTag();
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
         }
-        viewHolder.date.setText(soy.getCollectDate()+" "+ soy.getCollectTime());
+        viewHolder.date.setText(soy.getCollectDate() + " " + soy.getCollectTime());
         viewHolder.name.setText(soy.getCollectName());
         viewHolder.delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DBHelper.delete(soy);
-                mList.remove(soy);
-                notifyDataSetChanged();
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setMessage("确认要删除本条记录吗？");
+                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        DBHelper.delete(soy);
+                        mList.remove(soy);
+                        notifyDataSetChanged();
+                    }
+                });
+                builder.setNegativeButton("取消", null);
+                builder.create().show();
             }
         });
         return view;
     }
 
-    class ViewHolder{
+    class ViewHolder {
         TextView date;
         TextView name;
         Button delete;
